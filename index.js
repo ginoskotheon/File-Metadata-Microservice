7c5637fdc6f54c51fbf720c30a60f195beb731c6
William McDonald <ginoskotheon@yahoo.com>
2016-10-03 11:10:54 -0500

    port added

--- index.js
+++ index.js
@@ -12,4 +12,9 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+var port = process.env.PORT || 8080;
 
+
+app.listen(port, function(){
+    console.log('Listening on port ' + port);
+});
