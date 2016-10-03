6b51b87763b2d93166cc654fb870f79e152f506e
William McDonald <ginoskotheon@yahoo.com>
2016-10-03 11:49:14 -0500

    fixes

--- index.js
+++ index.js
@@ -12,6 +12,31 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.post('/', multer({ dest: './uploads/'}).single('userFile'), function(req,res){
+	console.log(req.body); //form fields
+	/* example output:
+	{ title: 'abc' }
+	 */
+	console.log(req.file); //form files
+	/* example output:
+            { fieldname: 'upl',
+              originalname: 'grumpy.png',
+              encoding: '7bit',
+              mimetype: 'image/png',
+              destination: './uploads/',
+              filename: '436ec561793aa4dc475a88e84776b1b9',
+              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+              size: 277056 }
+	 */
+    var ans = {
+        size: req.file.size
+    }
+    res.send(ans);
+	res.status(204).end();
+});
+
+
+
 var port = process.env.PORT || 8080;
 
 
